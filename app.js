6717e8a5c44d826f036679bd93cfbde3364efe80
Anubha Agrawal <anubhakannnu@gmail.com>
2020-02-23 12:34:50 +0530

    Changed PORT

--- app.js
+++ app.js
@@ -103,6 +103,7 @@ app.get("*", function(req, res){
 })
 
 // server listening on port 3000
-app.listen(3000, function(){
+PORT = process.env.PORT || 3000;
+app.listen(PORT, function(){
     console.log("Server started on port 3000");
 });
